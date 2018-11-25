d562f811710bdd5bd61fb9eb99852f847f90e7e3
y0natancohen <yonayonatan@gmail.com>
2018-11-25 21:54:44 +0200

    project structure

---
+++ src/test/java/bgu/spl/mics/application/passiveObjects/InventoryTest.java
@@ -0,0 +1,38 @@
+package bgu.spl.mics.application.passiveObjects;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class InventoryTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void getInstance() {
+    }
+
+    @Test
+    public void load() {
+    }
+
+    @Test
+    public void take() {
+    }
+
+    @Test
+    public void checkAvailabiltyAndGetPrice() {
+    }
+
+    @Test
+    public void printInventoryToFile() {
+    }
+}
