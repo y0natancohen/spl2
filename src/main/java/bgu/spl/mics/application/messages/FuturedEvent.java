fd351dd5125f4993ba5802ac42530ad83fac25ef
y0natancohen <yonayonatan@gmail.com>
2018-12-12 15:07:29 +0200

    merge

---
+++ src/main/java/bgu/spl/mics/application/messages/FuturedEvent.java
@@ -0,0 +1,32 @@
+package bgu.spl.mics.application.messages;
+
+import bgu.spl.mics.Event;
+import bgu.spl.mics.Future;
+
+import java.util.Objects;
+
+public class FuturedEvent<T> implements Event<T> {
+    private Future<T> future;
+
+    public Future<T> getFuture() {
+        return future;
+    }
+
+    public void setFuture(Future<T> future) {
+        this.future = future;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        FuturedEvent<?> baseEvent = (FuturedEvent<?>) o;
+        return Objects.equals(future, baseEvent.future);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(future);
+    }
+}
